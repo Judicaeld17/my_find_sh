2c0ebd69638b2452ad6bd88c39bb5a13d0101e82
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-03-27 22:42:17 +0100

    first commit

---
+++ my_find_sh.sh
@@ -0,0 +1,3 @@
+find . -type f -name "*.sh" -exec basename [] .sh
+# Description
+# Write a command line that searches for all file names that end with ".sh" (without quotation marks) in the current directory and all its sub-directories. It should display only the name of files without the extension .sh.
